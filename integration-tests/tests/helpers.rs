7694f6b8e93c0e38a8b93419a77db706e63b89b6
George Ornbo <george@shapeshed.com>
2023-06-20 13:54:27 +0100

    Update instantiation test

--- integration-tests/tests/helpers.rs
+++ integration-tests/tests/helpers.rs
@@ -10,29 +10,182 @@ use osmosis_test_tube::{Account, ExecuteResponse, OsmosisTestApp, Runner, Signin
 pub mod osmosis {
     use std::fmt::Display;
 
+    use cosmwasm_schema::cw_serde;
+    use cosmwasm_std::Decimal;
+    use osmosis_vault::msg::InstantiateMsg;
+    use simple_vault::state::ConfigUnchecked;
+    const OSMOSIS_VAULT_CONTRACT_NAME: &str = "osmosis_vault";
+
+    // Needed as liquidity_helper doesn't expose InstantiateMsg type
+    #[cw_serde]
+    pub struct BlankInstantiateMsg {}
+    use apollo_cw_asset::{AssetInfo, AssetInfoUnchecked};
     use cosmwasm_std::Coin;
-    use osmosis_test_tube::{OsmosisTestApp, RunnerError, SigningAccount, Wasm};
+    use cw_dex::{osmosis::OsmosisPool, traits::Pool as PoolTrait};
+    use cw_dex_router::{
+        msg::ExecuteMsg,
+        operations::{SwapOperation, SwapOperationsList},
+    };
+    use osmosis_test_tube::{
+        Account, Gamm, Module, OsmosisTestApp, RunnerError, SigningAccount, Wasm,
+    };
     use serde::Serialize;
 
     pub struct Setup {
         pub app: OsmosisTestApp,
         pub signer: SigningAccount,
+        pub admin: SigningAccount,
+        pub force_withdraw_admin: SigningAccount,
+        pub treasury: SigningAccount,
+        pub vault_address: String,
+        pub base_token: String,
     }
 
     impl Setup {
         pub fn new() -> Self {
             let app = OsmosisTestApp::new();
+            let wasm = Wasm::new(&app);
+            let gamm = Gamm::new(&app);
 
             let signer = app
+                .init_account(&[
+                    Coin::new(1_000_000_000_000, "uatom"),
+                    Coin::new(1_000_000_000_000, "uosmo"),
+                    Coin::new(1_000_000_000_000, "pica"),
+                ])
+                .unwrap();
+
+            let admin = app
                 .init_account(&[
                     Coin::new(1_000_000_000_000, "uatom"),
                     Coin::new(1_000_000_000_000, "uosmo"),
                 ])
                 .unwrap();
 
+            let force_withdraw_admin = app
+                .init_account(&[
+                    Coin::new(1_000_000_000_000, "uatom"),
+                    Coin::new(1_000_000_000_000, "uosmo"),
+                ])
+                .unwrap();
+
+            let treasury = app
+                .init_account(&[
+                    Coin::new(1_000_000_000_000, "uatom"),
+                    Coin::new(1_000_000_000_000, "uosmo"),
+                ])
+                .unwrap();
+
+            // Set performance fee to 0.125
+            let performance_fee = Decimal::permille(125);
+
+            // Base pool uatom / uosmo
+            let pool_liquidity = vec![Coin::new(1_000, "uatom"), Coin::new(1_000, "uosmo")];
+            let base_pool_id =
+                gamm.create_basic_pool(&pool_liquidity, &signer).unwrap().data.pool_id;
+
+            let base_pool = OsmosisPool::unchecked(base_pool_id);
+            let base_token = base_pool.lp_token();
+
+            // Setup reward token as Pica and liquidity pool
+            let reward_token_denoms = vec!["pica".to_string()];
+            let reward_liquidation_target = "uatom".to_string();
+            let reward1_pool_liquidity = vec![Coin::new(1_000, "pica"), Coin::new(1_000, "uatom")];
+            let reward1_pool_id =
+                gamm.create_basic_pool(&reward1_pool_liquidity, &signer).unwrap().data.pool_id;
+            let reward1_pool = OsmosisPool::unchecked(reward1_pool_id);
+            let reward1_token = reward1_pool_liquidity
+                .iter()
+                .find(|x| x.denom != reward_liquidation_target)
+                .unwrap()
+                .denom
+                .clone();
+
+            let reward_assets = reward_token_denoms
+                .iter()
+                .map(|x| AssetInfoUnchecked::Native(x.clone()))
+                .collect::<Vec<_>>();
+
+            let olh_wasm_byte_code = std::fs::read(
+                "../integration-tests/tests/test-artifacts/osmosis_liquidity_helper.wasm",
+            )
+            .unwrap();
+            let olh_code_id =
+                wasm.store_code(&olh_wasm_byte_code, None, &admin).unwrap().data.code_id;
+
+            let osmosis_liquidity_helper = wasm
+                .instantiate(olh_code_id, &BlankInstantiateMsg {}, None, None, &[], &admin)
+                .unwrap()
+                .data
+                .address;
+
+            let cw_dex_wasm_byte_code = std::fs::read(
+                "../integration-tests/tests/test-artifacts/cw_dex_router_osmosis.wasm",
+            )
+            .unwrap();
+            let cw_dex_code_id =
+                wasm.store_code(&cw_dex_wasm_byte_code, None, &admin).unwrap().data.code_id;
+
+            let router_address = wasm
+                .instantiate(cw_dex_code_id, &BlankInstantiateMsg {}, None, None, &[], &admin)
+                .unwrap()
+                .data
+                .address;
+
+            let lh = liquidity_helper::helper::LiquidityHelperBase(osmosis_liquidity_helper);
+
+            let config = ConfigUnchecked {
+                force_withdraw_whitelist: vec![force_withdraw_admin.address()],
+                performance_fee,
+                reward_assets,
+                reward_liquidation_target: AssetInfoUnchecked::Native(
+                    reward_liquidation_target.clone(),
+                ),
+                treasury: treasury.address(),
+                liquidity_helper: lh,
+                router: router_address.clone().into(),
+            };
+
+            // Update path on the router
+            wasm.execute(
+                &router_address,
+                &ExecuteMsg::SetPath {
+                    offer_asset: AssetInfo::Native(reward1_token.clone()).into(),
+                    ask_asset: AssetInfo::Native(reward_liquidation_target.clone()).into(),
+                    path: SwapOperationsList::new(vec![SwapOperation {
+                        offer_asset_info: AssetInfo::Native(reward1_token),
+                        ask_asset_info: AssetInfo::Native(reward_liquidation_target),
+                        pool: cw_dex::Pool::Osmosis(reward1_pool),
+                    }])
+                    .into(),
+                    bidirectional: false,
+                },
+                &[],
+                &admin,
+            )
+            .unwrap();
+
+            let vault_address = instantiate_contract(
+                &wasm,
+                &signer,
+                OSMOSIS_VAULT_CONTRACT_NAME,
+                &InstantiateMsg {
+                    admin: admin.address(),
+                    pool_id: base_pool_id,
+                    lockup_duration: 86400u64,
+                    config,
+                    vault_token_subdenom: "osmosis-vault".to_string(),
+                },
+            );
+
             Self {
                 app,
+                admin,
                 signer,
+                force_withdraw_admin,
+                treasury,
+                base_token: base_token.to_string(),
+                vault_address,
             }
         }
     }
--- integration-tests/tests/test_base_vault.rs
+++ integration-tests/tests/test_base_vault.rs
@@ -1,212 +1,81 @@
 mod helpers;
-use apollo_cw_asset::{AssetInfo, AssetInfoUnchecked};
-use cosmrs::proto::cosmos::bank::v1beta1::QueryBalanceRequest;
-use cosmwasm_schema::cw_serde;
-use cosmwasm_std::{Coin, Decimal};
-use cw_dex::osmosis::OsmosisPool;
-use cw_dex_router::{
-    msg::ExecuteMsg,
-    operations::{SwapOperation, SwapOperationsList},
+use apollo_cw_asset::AssetInfoBase;
+use cosmwasm_std::{Decimal, Uint128};
+use cw_dex::{
+    osmosis::{OsmosisPool, OsmosisStaking},
+    traits::Pool as PoolTrait,
 };
-use osmosis_test_tube::{Account, Bank, Gamm, Module, OsmosisTestApp, Wasm};
-use osmosis_vault::msg::InstantiateMsg;
-use simple_vault::state::ConfigUnchecked;
+use cw_vault_token::osmosis::OsmosisDenom;
+use osmosis_test_tube::{Account, Module, Wasm};
+use osmosis_vault::msg::QueryMsg;
+use simple_vault::msg::{ExtensionQueryMsg, SimpleExtensionQueryMsg, StateResponse};
 
-use crate::helpers::osmosis::{instantiate_contract, Setup};
-
-// Needed as liquidity_helper doesn't expose InstantiateMsg type
-#[cw_serde]
-pub struct BlankInstantiateMsg {}
-
-const OSMOSIS_VAULT_CONTRACT_NAME: &str = "osmosis_vault";
+use crate::helpers::osmosis::Setup;
 
 #[test]
 fn instantiation() {
-    let app = OsmosisTestApp::new();
+    let Setup {
+        app,
+        signer: _,
+        admin,
+        force_withdraw_admin,
+        treasury,
+        vault_address,
+        base_token,
+    } = Setup::new();
+
     let wasm = Wasm::new(&app);
-    let gamm = Gamm::new(&app);
 
-    let signer = app
-        .init_account(&[
-            Coin::new(1_000_000_000_000, "uatom"),
-            Coin::new(1_000_000_000_000, "uosmo"),
-            Coin::new(1_000_000_000_000, "pica"),
-        ])
+    let state: StateResponse<OsmosisStaking, OsmosisPool, OsmosisDenom> = wasm
+        .query(
+            &vault_address,
+            &QueryMsg::VaultExtension(ExtensionQueryMsg::Simple(SimpleExtensionQueryMsg::State {})),
+        )
         .unwrap();
 
-    let admin = app
-        .init_account(&[
-            Coin::new(1_000_000_000_000, "uatom"),
-            Coin::new(1_000_000_000_000, "uosmo"),
-        ])
-        .unwrap();
+    let vault_token_denom = state.vault_token.to_string();
+    let total_staked_base_tokens = state.total_staked_base_tokens;
+    let vault_token_supply = state.vault_token_supply;
+    let vault_admin = state.admin;
+    let config = state.config;
+    let pool = state.pool;
 
-    let force_withdraw_admin = app
-        .init_account(&[
-            Coin::new(1_000_000_000_000, "uatom"),
-            Coin::new(1_000_000_000_000, "uosmo"),
-        ])
-        .unwrap();
+    // Check admin address is set correctly
+    assert_eq!(vault_admin.unwrap(), admin.address());
 
-    let treasury = app
-        .init_account(&[
-            Coin::new(1_000_000_000_000, "uatom"),
-            Coin::new(1_000_000_000_000, "uosmo"),
-        ])
-        .unwrap();
+    // Check the config of the vault is set correctly
+    // Performance fee is 0.125
+    assert_eq!(config.performance_fee, Decimal::permille(125));
+    // Treasury address is correct
+    assert_eq!(config.treasury, treasury.address());
+    // Router address is correct
+    // assert_eq!(config.router, TODO);
+    // Reward asset is set correctly
+    assert_eq!(config.reward_assets, vec![AssetInfoBase::Native("pica".to_string())]);
+    // Reward liquidation target is set correctly
+    assert_eq!(config.reward_liquidation_target, AssetInfoBase::Native("uatom".to_string()));
+    // Whitelisted addresses that can call ForceWithdraw and
+    // ForceWithdrawUnlocking are set correctly
+    assert_eq!(config.force_withdraw_whitelist, vec![force_withdraw_admin.address()]);
+    // Liquidity helper address is set correctly
+    // assert_eq!(config.liquidity_helper, TODO);
 
-    // Set performance fee to 0.125
-    let performance_fee = Decimal::permille(125);
+    // Check staked tokens is zero
+    assert_eq!(total_staked_base_tokens, Uint128::zero());
 
-    // Base pool uatom / uosmo
-    let pool_liquidity = vec![Coin::new(1_000, "uatom"), Coin::new(1_000, "uosmo")];
-    let base_pool_id = gamm.create_basic_pool(&pool_liquidity, &signer).unwrap().data.pool_id;
+    // TODO Check the Staking struct is set correctly
 
-    // Setup reward token as Pica and liquidity pool
-    let reward_token_denoms = vec!["pica".to_string()];
-    let reward_liquidation_target = "uatom".to_string();
-    let reward1_pool_liquidity = vec![Coin::new(1_000, "pica"), Coin::new(1_000, "uatom")];
-    let reward1_pool_id =
-        gamm.create_basic_pool(&reward1_pool_liquidity, &signer).unwrap().data.pool_id;
-    let reward1_pool = OsmosisPool::unchecked(reward1_pool_id);
-    let reward1_token = reward1_pool_liquidity
-        .iter()
-        .find(|x| x.denom != reward_liquidation_target)
-        .unwrap()
-        .denom
-        .clone();
+    // Check the Pool struct is set correctly
+    assert_eq!(pool.lp_token().to_string(), base_token);
 
-    let reward_assets = reward_token_denoms
-        .iter()
-        .map(|x| AssetInfoUnchecked::Native(x.clone()))
-        .collect::<Vec<_>>();
-
-    let olh_wasm_byte_code =
-        std::fs::read("../integration-tests/tests/test-artifacts/osmosis_liquidity_helper.wasm")
-            .unwrap();
-    let olh_code_id = wasm.store_code(&olh_wasm_byte_code, None, &admin).unwrap().data.code_id;
-
-    let osmosis_liquidity_helper = wasm
-        .instantiate(olh_code_id, &BlankInstantiateMsg {}, None, None, &[], &admin)
-        .unwrap()
-        .data
-        .address;
-
-    let cw_dex_wasm_byte_code =
-        std::fs::read("../integration-tests/tests/test-artifacts/cw_dex_router_osmosis.wasm")
-            .unwrap();
-    let cw_dex_code_id =
-        wasm.store_code(&cw_dex_wasm_byte_code, None, &admin).unwrap().data.code_id;
-
-    let cw_dex_router = wasm
-        .instantiate(cw_dex_code_id, &BlankInstantiateMsg {}, None, None, &[], &admin)
-        .unwrap()
-        .data
-        .address;
-
-    let lh = liquidity_helper::helper::LiquidityHelperBase(osmosis_liquidity_helper);
-
-    let config = ConfigUnchecked {
-        force_withdraw_whitelist: vec![force_withdraw_admin.address()],
-        performance_fee,
-        reward_assets,
-        reward_liquidation_target: AssetInfoUnchecked::Native(reward_liquidation_target.clone()),
-        treasury: treasury.address(),
-        liquidity_helper: lh,
-        router: cw_dex_router.clone().into(),
-    };
-
-    // Update path on the router
-    wasm.execute(
-        &cw_dex_router,
-        &ExecuteMsg::SetPath {
-            offer_asset: AssetInfo::Native(reward1_token.clone()).into(),
-            ask_asset: AssetInfo::Native(reward_liquidation_target.clone()).into(),
-            path: SwapOperationsList::new(vec![SwapOperation {
-                offer_asset_info: AssetInfo::Native(reward1_token),
-                ask_asset_info: AssetInfo::Native(reward_liquidation_target),
-                pool: cw_dex::Pool::Osmosis(reward1_pool),
-            }])
-            .into(),
-            bidirectional: false,
-        },
-        &[],
-        &admin,
-    )
-    .unwrap();
-
-    let _contract_addr = instantiate_contract(
-        &wasm,
-        &signer,
-        OSMOSIS_VAULT_CONTRACT_NAME,
-        &InstantiateMsg {
-            admin: admin.address(),
-            pool_id: base_pool_id,
-            lockup_duration: 86400u64,
-            config,
-            vault_token_subdenom: "osmosis-vault".to_string(),
-        },
+    // Check the vault token is set correctly
+    // TODO replace string with regex
+    assert_eq!(
+        vault_token_denom,
+        "factory/osmo17p9rzwnnfxcjp32un9ug7yhhzgtkhvl9jfksztgw5uh69wac2pgs5yczr8/osmosis-vault"
+            .to_string()
     );
-}
 
-#[test]
-fn test_pool_setup() {
-    let Setup {
-        app,
-        signer,
-    } = Setup::new();
-
-    let gamm = Gamm::new(&app);
-    let bank = Bank::new(&app);
-
-    let pool_liquidity = vec![Coin::new(1_000, "uatom"), Coin::new(1_000, "uosmo")];
-    let pool_id = gamm.create_basic_pool(&pool_liquidity, &signer).unwrap().data.pool_id;
-
-    let pool = gamm.query_pool(pool_id).unwrap();
-
-    let lp_token = pool.total_shares.unwrap().denom;
-    assert_eq!(lp_token, "gamm/pool/1".to_string());
-
-    let signer_balance = bank
-        .query_balance(&QueryBalanceRequest {
-            address: signer.address(),
-            denom: lp_token,
-        })
-        .unwrap()
-        .balance
-        .unwrap()
-        .amount;
-
-    assert_eq!(signer_balance, "100000000000000000000".to_string());
-}
-
-#[test]
-fn test_deposit() {
-    let Setup {
-        app,
-        signer,
-    } = Setup::new();
-
-    let gamm = Gamm::new(&app);
-    let bank = Bank::new(&app);
-
-    let pool_liquidity = vec![Coin::new(1_000, "uatom"), Coin::new(1_000, "uosmo")];
-    let pool_id = gamm.create_basic_pool(&pool_liquidity, &signer).unwrap().data.pool_id;
-
-    let pool = gamm.query_pool(pool_id).unwrap();
-
-    let lp_token = pool.total_shares.unwrap().denom;
-    assert_eq!(lp_token, "gamm/pool/1".to_string());
-
-    let signer_balance = bank
-        .query_balance(&QueryBalanceRequest {
-            address: signer.address(),
-            denom: lp_token,
-        })
-        .unwrap()
-        .balance
-        .unwrap()
-        .amount;
-
-    assert_eq!(signer_balance, "100000000000000000000".to_string());
+    // Check vault token supply is zero
+    assert_eq!(vault_token_supply, Uint128::zero());
 }
