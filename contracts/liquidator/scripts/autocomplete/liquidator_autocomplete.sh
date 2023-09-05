339facac41e88ce20fa163af18116a0c2c5d3c1e
George Ornbo <george@shapeshed.com>
2023-09-05 18:21:40 +0100

    Add autocomplete files

---
+++ contracts/liquidator/scripts/autocomplete/liquidator_autocomplete.sh
@@ -0,0 +1,31 @@
+_liquidator_autocomplete() {
+  local cur prev opts base
+  COMPREPLY=()
+  cur="${COMP_WORDS[COMP_CWORD]}"
+  prev="${COMP_WORDS[COMP_CWORD - 1]}"
+
+  # Base command options
+  opts="help version query store execute instantiate"
+
+  if [[ ${cur} == -* || ${COMP_CWORD} -eq 1 ]]; then
+    COMPREPLY=($(compgen -W "${opts}" -- ${cur}))
+    return 0
+  fi
+
+  # Handle subcommands
+  case "${prev}" in
+    query)
+      local subopts="config state owner route"
+      COMPREPLY=($(compgen -W "${subopts}" -- ${cur}))
+      ;;
+    execute)
+      local subopts="route config owner liquidate ibc_transfer"
+      COMPREPLY=($(compgen -W "${subopts}" -- ${cur}))
+      ;;
+    *) ;;
+  esac
+
+  return 0
+}
+
+complete -F _liquidator_autocomplete liquidator
