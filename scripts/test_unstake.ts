c51f5b1c802b0b5bfadf6815c3c0b4ab4e90ee7f
Lucas Marc <lucas@thinkanddev.com>
2023-12-29 10:45:43 -0300

    feat: minor fixes and optimizations

--- scripts/test_unstake.ts
+++ scripts/test_unstake.ts
@@ -1,29 +1,26 @@
 import "dotenv/config.js";
-import { executeContract, queryContract } from "./helpers.js";
-import { GasPrice, setupNodeLocal, Coin } from "cosmwasm";
+import { queryContract } from "./helpers.js";
+import { GasPrice, setupNodeLocal } from "cosmwasm";
 import { DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
 import { toUtf8, toBase64 } from "@cosmjs/encoding";
-import { testnet } from "./deploy_configs.js";
 import readline from "readline";
 
-function waitForInput(): Promise<void> {
+function waitForInput(question: string = ""): Promise<string> {
   return new Promise((resolve) => {
     const rl = readline.createInterface({
       input: process.stdin,
       output: process.stdout,
     });
 
-    console.log("Press any key to continue...");
-
-    rl.question("", () => {
+    if (question === "") {
+      console.log("Press any key to continue...");
+    }
+    rl.question(question, (answer: string) => {
       rl.close();
-      resolve();
+      resolve(answer);
     });
   });
 }
-
-// consts
-const ARTIFACTS_PATH = "../artifacts";
 
 function createConfig(
   chainId?: string,
@@ -46,6 +43,10 @@ function createConfig(
   return { chainId, rpcEndpoint, prefix, gasPrice };
 }
 
+const jsonToBinary = (json: Record<string, unknown>): string => {
+  return toBase64(toUtf8(JSON.stringify(json)));
+};
+
 // main
 async function main() {
   const mnemonic = process.env.MNEMONIC;
@@ -67,17 +68,11 @@ async function main() {
   const wallet = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic, {
     prefix: config.prefix,
   });
-  let deployConfig: Config = testnet;
   const isTestnet = process.env.NETWORK === "testnet";
 
   const [account] = await wallet.getAccounts();
 
   console.log(`\nWallet address from seed (owner): ${account.address}`);
-
-  // ///
-  // /// Query staking config
-  // ///
-  // await waitForInput();
 
   const stakingDeploymentAddress =
     "neutron1qjxme67x6nq3j623kjs5utjwd20npxhs2493fpgmxa05868y4pmqqhe06w";
@@ -91,41 +86,38 @@ async function main() {
   });
   console.log("State:\n", state);
   console.log("Config:\n", stakingConfig);
-  await waitForInput();
+  let amount_to_unstake: string;
+  // keep asking for input until we get a valid amount
+  while (
+    isNaN(
+      Number(
+        (amount_to_unstake = await waitForInput(
+          "\nHow many tokens would you like to unstake (in untrn)?\n"
+        ))
+      )
+    )
+  ) {
+    console.log("Invalid amount, try again.");
+  }
 
   let msg = {
     unstake: {},
   };
-  const jsonToBinary = (json: Record<string, unknown>): string => {
-    return toBase64(toUtf8(JSON.stringify(json)));
-  };
+
   let unstake_tx = await client.execute(
     account.address,
     stakingConfig.staked_denom,
     {
       send: {
-        contract:
-          "neutron1qjxme67x6nq3j623kjs5utjwd20npxhs2493fpgmxa05868y4pmqqhe06w",
-        amount: "1000",
+        contract: stakingDeploymentAddress,
+        amount: amount_to_unstake,
         msg: jsonToBinary(msg),
       },
     },
     "auto" // Will simulate and estimate gas automatically
-    //{ amount: [{ denom: "untrn", amount: "35050" }], gas: "232111" } // Force broadcasting to the blockchain
+    // { amount: [{ denom: "untrn", amount: "8200" }], gas: "410000" } // Force broadcasting to the blockchain
   );
 
-  // const result = await client.execute(
-  //   senderAddress,
-  //   contractAddress,
-  //   { send: { recipient, amount, msg: jsonToBinary(msg) } },
-  //   fee,
-  // )
-
-  //   &Cw20ExecuteMsg::Send {
-  //     contract: staking_address,
-  //     amount: amount_to_unstake.into(),
-  //     msg: to_binary(&Cw20HookMsg::Unstake {}).unwrap(),
-  // },
   console.log("\nUnstaking tx:\n\t", unstake_tx.transactionHash);
 }
 
